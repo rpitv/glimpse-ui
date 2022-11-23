aae9239177d5ad5c8057df623d1141b6df076d9e
Erik Roberts <me@ecr.dev>
2022-11-22 21:00:46 -0500

    Add start of the individual production view

---
+++ src/views/ProductionView.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <ProductionCarousel />
+  <h1>{{ production.result.value?.ReadProduction?.name }}</h1>
+  <span>
+    {{production.result.value?.ReadProduction?.category?.name}}
+    •
+    {{ production.result.value?.ReadProduction.startTime }}
+  </span>
+  <p>{{production.result.value?.ReadProduction.description}}</p>
+  <ProductionCredits />
+  <ProductionTags />
+</div>
+</template>
+
+<script setup lang="ts">
+
+import {useRoute} from "vue-router";
+import {useQuery} from "@vue/apollo-composable";
+import {ReadProductionDocument} from "@/graphql/types";
+import ProductionTags from "@/components/production/ProductionTags.vue";
+import ProductionCredits from "@/components/production/ProductionCredits.vue";
+import ProductionCarousel from "@/components/production/ProductionCarousel.vue";
+
+const route = useRoute();
+
+const production = useQuery(ReadProductionDocument, {
+  id: route.params.id.toString()
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
