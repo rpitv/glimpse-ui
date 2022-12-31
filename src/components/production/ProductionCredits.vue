43dbc948bf0a2764d4b78e2fbbe5e63d1b98da50
Erik Roberts <me@ecr.dev>
2022-12-30 19:08:11 -0500

    Update credits to be sorted and remove profile picture (for now)

--- src/components/production/ProductionCredits.vue
+++ src/components/production/ProductionCredits.vue
@@ -1,13 +1,9 @@
 <template>
   <h3>Credits</h3>
     <n-grid :cols="3">
-        <n-grid-item v-for="credit in credits" :key="credit.id" class="col">
+        <n-grid-item v-for="credit in sortedCredits" :key="credit.id" class="col">
           <!--TODO-->
           <span class="no-display-at-1250">&ensp; </span>
-          <n-image v-if="credit.person.images" :src="credit.person.images[0].image" class="profile-icon" width="25"/>
-          <n-image v-else width="25" class="profile-icon"
-            src="https://img.freepik.com/premium-vector/male-avatar-icon-unknown-anonymous-person-default-avatar-profile-icon-social-media-user-business-man-man-profile-silhouette-isolated-white-background-vector-illustration_735449-120.jpg"
-          />
           <span class="name">&nbsp;{{ credit.person.name }}</span>
           <span class="no-display-at-1250"> - </span>
           <span class="title">{{ credit.title }}</span>
@@ -18,13 +14,18 @@
 <script setup lang="ts">
 import type {PropType} from "vue";
 import type {Credit} from "@/graphql/types";
-import {NGrid, NGridItem, NImage} from "naive-ui";
+import {NGrid, NGridItem} from "naive-ui";
+import {computed} from "vue";
 
 const props = defineProps({
   credits: {
     type: Array as PropType<Pick<Credit, "__typename" | "title" | "priority" | "person" | "id">[]>,
     required: true
   }
+});
+
+const sortedCredits = computed(() => {
+  return [...props.credits].sort((a, b) => b.priority - a.priority);
 });
 
 </script>
