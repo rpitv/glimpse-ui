70a4aaf4c6790338d1eb1d9d8fb31447f8535eab
Billy Wu <83081917+SuperUltraMegaChicken@users.noreply.github.com>
2023-01-08 00:13:42 -0500

    Started AboutView of the website
    Unfinished and committed to work on a different device :smile:

---
+++ src/components/PersonDisplay.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{name}}
+    <p>
+      {{role}}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,57 +1,74 @@
 <template>
   <div class="container">
     <h1 class="title">
-      About
+      About Us
     </h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac orci vestibulum, fermentum lorem accumsan,
-      aliquet turpis. Donec lectus diam, tincidunt ut eleifend et, dignissim at ipsum. Integer interdum luctus
-      sapien, in finibus leo condimentum lobortis. Sed nibh mauris, semper at magna sit amet, ornare pulvinar
-      mauris. Duis fermentum, mauris at tristique volutpat, metus enim accumsan augue, ut semper urna ipsum et
-      nisl. Sed non commodo elit. Vestibulum lacinia leo facilisis libero faucibus ullamcorper quis nec eros.
-      Nulla vitae eleifend elit. Sed lacinia pellentesque quam a ultrices. Nam nisi neque, tincidunt in varius
-      sed, tempor quis est. Suspendisse odio leo, pellentesque dictum mattis sed, tincidunt nec nunc. Vivamus id
-      rhoncus mi. Integer ac finibus nunc. Etiam eget euismod nisl. Aliquam non tempus nisi, a gravida magna. Morbi
-      vitae tellus nisl.
-    </p>
-    <p>
-      Vivamus in eros augue. Suspendisse non semper dolor, ac lobortis purus. Mauris iaculis, erat vitae ultrices
-
-      convallis, nisl nunc faucibus ante, in gravida ante magna id nisi. Interdum et malesuada fames ac ante ipsum
-      primis in faucibus. Etiam laoreet risus nulla, ac viverra erat ornare non. Donec eros dui, bibendum ac tortor
-      et, lobortis euismod augue. Fusce et leo augue. Sed ac feugiat augue, interdum pharetra quam. Mauris mi enim,
-      commodo eu finibus id, pulvinar at massa. Sed tincidunt est egestas tristique tincidunt. Sed sed dolor sapien.
-      Phasellus nec diam faucibus, rhoncus purus ac, luctus massa. Nullam nisi metus, commodo sit amet placerat non,
-      tincidunt eget nunc. Aenean consequat massa nec justo facilisis ullamcorper. In scelerisque tempor lacinia.
-    </p>
-    <p>
-      Vestibulum tincidunt fermentum diam et volutpat. Etiam accumsan iaculis varius. Sed porta eu ipsum nec
-      consectetur. Cras placerat risus sit amet nisl consequat, congue euismod nibh porttitor. Aliquam et turpis ut
-      velit malesuada dignissim ac quis diam. Phasellus et ipsum urna. Cras pulvinar molestie euismod.
-    </p>
-    <p>
-      Suspendisse fermentum maximus magna, tincidunt tincidunt ligula pulvinar semper. Mauris libero massa,
-      aliquet id ante quis, rhoncus consequat enim. Sed laoreet odio elit, ac dictum nunc rutrum tempor. Praesent
-      at erat suscipit, gravida massa sit amet, convallis nulla. Mauris at justo pharetra, ultricies sapien et,
-      malesuada diam. Sed eget elit non diam molestie pharetra. Mauris sit amet vulputate ante.
-    </p>
-    <p>
-      Aliquam vestibulum nisi elit, congue vestibulum tortor dictum vitae. Vestibulum interdum arcu sit amet
-      vehicula fringilla. Cras pulvinar vehicula est at iaculis. Aliquam elementum nibh lorem, non cursus mi
-      posuere in. Ut quis lacus mi. Curabitur vehicula neque sit amet justo feugiat tempus. Curabitur urna
-      ligula, hendrerit vitae vestibulum non, aliquam et orci. Mauris tristique sed nulla id mollis. Nam
-      dignissim laoreet velit, in mollis leo vestibulum id. Morbi nec magna elit. Maecenas malesuada nec elit
-      vitae dictum. Duis venenatis congue leo. Aliquam vel ante vel lorem ullamcorper elementum sed a ipsum. Nam
-      eros lacus, maximus vitae urna id, consequat elementum sem. Donec in ante orci. Nam aliquet libero turpis,
-      quis malesuada ex eleifend a.
-    </p>
+    <n-grid x-gap="10" :cols="2">
+      <n-grid-item>
+        <p class="info">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac orci vestibulum, fermentum lorem accumsan,
+          aliquet turpis. Donec lectus diam, tincidunt ut eleifend et, dignissim at ipsum. Integer interdum luctus
+          sapien, in finibus leo condimentum lobortis. Sed nibh mauris, semper at magna sit amet, ornare pulvinar
+          mauris. Duis fermentum, mauris at tristique volutpat, metus enim accumsan augue, ut semper urna ipsum et
+          nisl.
+        </p>
+      </n-grid-item>
+      <n-grid-item>
+        <iframe class="player" src="https://www.youtube.com/embed/MfzmM3EFqIY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </n-grid-item>
+    </n-grid>
+    <h1>
+      Policy
+    </h1>
+    <ul>
+      <li>RPI TV Constitution</li>
+      <li>Rensselaer Union Media Statement</li>
+    </ul>
+    <h1>Our Team</h1>
+    <h2>
+      <ul>Leadership
+      <div v-for="person in persons.result.value.findManyPerson">
+        <PersonDisplay role="president" :name="person.name" />
+      </div>
+      </ul>
+      <ul>Members</ul>
+    </h2>
   </div>
 </template>
 
 <script setup lang="ts">
+import {NGrid, NGridItem} from "naive-ui";
+import {useQuery} from "@vue/apollo-composable";
+import gql from "graphql-tag"
+import PersonDisplay from "@/components/PersonDisplay.vue";
 
+const persons = useQuery(gql`
+    query MyQuery {
+      findManyPerson {
+        roles {
+          name
+          priority
+        }
+        name
+      }
+    }`);
+
+console.log(persons.result.value.findManyPerson);
 </script>
 
 <style scoped lang="scss">
+
+.info {
+  width: 95%;
+}
+
+.player {
+  aspect-ratio: 16/9;
+  width: 90%;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+}
 
 </style>
